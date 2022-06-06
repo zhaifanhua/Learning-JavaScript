14058d8b69f495e5fa4f2db407f541853deecfae
zhaifanhua <zhaifanhua@foxmail.com>
2022-06-07 00:57:00 +0800

    新增：浏览器书签便捷脚本

---
+++ BookmarkScript/Github自动删除仓库.js
@@ -0,0 +1,12 @@
+(function () {
+    var repository_el = document.querySelector("#options_bucket > div.Box.color-border-danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > p:nth-child(2) > strong");
+    var input_el = document.querySelector("#options_bucket > div.Box.color-border-danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > form > p > input");
+    var submit_el = document.querySelector("#options_bucket > div.Box.color-border-danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > form > button");
+    if (confirm("开源不易，代码不易，确认删除【" + repository_el.textContent + "】这个仓库吗？摘繁华版权所有")) {
+        input_el.value = repository_el.textContent;
+        submit_el.disabled = false;
+        submit_el.click();
+        alert("好狠！仓库【" + repository_el.textContent + "】即将删除！");
+    } else
+        alert("果然你还是爱惜自己的成果，已取消删除仓库【" + repository_el.textContent + "】。");
+})();
---
+++ BookmarkScript/rademe.md
@@ -0,0 +1,6 @@
+新建浏览器书签，复制js代码，并在最前面加上
+
+```javascript
+javascript:
+```
+
