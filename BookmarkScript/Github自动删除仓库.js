defef20d0824634e7880544f1887390e06faab64
zhaifanhua <zhaifanhua@foxmail.com>
2022-06-07 01:13:37 +0800

    Github仓库删除脚本把按钮禁用关了就行了，不必那么麻烦

--- BookmarkScript/Github自动删除仓库.js
+++ BookmarkScript/Github自动删除仓库.js
@@ -1,9 +1,9 @@
 (function () {
     var repository_el = document.querySelector("#options_bucket > div.Box.color-border-danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > p:nth-child(2) > strong");
-    var input_el = document.querySelector("#options_bucket > div.Box.color-border-danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > form > p > input");
+    // var input_el = document.querySelector("#options_bucket > div.Box.color-border-danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > form > p > input");
     var submit_el = document.querySelector("#options_bucket > div.Box.color-border-danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > form > button");
     if (confirm("开源不易，代码不易，确认删除【" + repository_el.textContent + "】这个仓库吗？摘繁华版权所有")) {
-        input_el.value = repository_el.textContent;
+        // input_el.value = repository_el.textContent;
         submit_el.disabled = false;
         submit_el.click();
         alert("好狠！仓库【" + repository_el.textContent + "】即将删除！");
